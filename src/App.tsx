1e38201eaa98e1c0ac074d8954d9cb8aefbf16e2
mryevhenii07 <mrit.works07@gmail.com>
2023-01-23 12:56:31 +0200

    layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 
-import './App.css';
+import './scss/app.scss';
+import Header from './components/Header';
+import Sort from './components/Sort';
+import Categories from './components/Categories';
+import Pizza from './components/Pizza';
+
 
 function App() {
   return (
-    <div className="App">
-    Hello
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Всі піци</h2>
+          <div className="content__items">
+            <Pizza />
+
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Categories.tsx
@@ -0,0 +1,16 @@
+const Categories = () => {
+    return (
+        <div className="categories">
+            <ul>
+                <li className="active">Всі</li>
+                <li>Вегетаріанська</li>
+                <li>М'ясні</li>
+                <li>Гриль</li>
+                <li>Гострі</li>
+                <li>Закриті</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Categories
